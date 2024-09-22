d9f49681cba368792fb337157a698faa2687c38e
Mahalakshmi <mahakgm0827@gmail.com>
2024-09-22 22:28:52 +0530

    Changes

--- sample2/src/pages/Projects.jsx
+++ sample2/src/pages/Projects.jsx
@@ -1,63 +1,43 @@
-import Rentify from "../assets/img/Rentify.png";
-import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import ProjectCard from "../components/ProjectCard";
+import { getProjects } from "../services/api";
+import AddComponent from "../components/AddComponent";
+
 const Projects = () => {
+  const [projectdata, setProjectdata] = useState(null);
+  const fetchprojects = async () => {
+    // const {}
+    try {
+      const { data } = await getProjects();
+      setProjectdata(data);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+  console.log(projectdata);
+  useEffect(() => {
+    fetchprojects();
+  }, []);
+  if (!projectdata) {
+    return "No data available";
+  }
   return (
     <>
-      <div className=" min-h-screen h-[80%] flex  flex-col px-9 bg-[#73d6d3]">
-        <div className="flex justify-start items-start px-10 py-10">
-          <h1 className="font-bold text-4xl text-white">Projects</h1>
+      <div className="w-full h-full flex flex-row flex-wrap gap-8 justify-center items-center">
+        <div className="w-full h-[10%] flex justify-center items-center">
+          <AddComponent />
         </div>
-        <div className=" flex flex-row justify-center items-center">
-          <div className="flex flex-row justify-center items-center gap-10">
-            <div className="p-5 bg-white rounded-md shadow-md">
-              <img
-                src={Rentify}
-                className="h-[10rem] w-[100%] flex justify-center items-center"
-              />
-              <h3 className="text-2xl font-medium ">Rentify</h3>
-              <p className=" text-base">
-                It is Rental House Booking Website where users can rent a house
-                and also sellers can update the property
-              </p>
-            </div>
 
-            <div className="p-5 bg-white rounded-md shadow-md">
-              <img
-                src={Rentify}
-                className="h-[10rem]  w-[100%] flex justify-center items-center"
-              />
-              <h3 className="text-2xl font-medium ">Rentify</h3>
-              <p className=" text-base">
-                It is Rental House Booking Website where users can rent a house
-                and also sellers can update the property
-              </p>
-            </div>
-
-            <div className="p-5 bg-white rounded-md shadow-md">
-              <img
-                src={Rentify}
-                className="h-[10rem]  w-[100%] flex justify-center items-center"
-              />
-              <h3 className="text-2xl font-medium ">Rentify</h3>
-              <p className=" text-base">
-                It is Rental House Booking Website where users can rent a house
-                and also sellers can update the property
-              </p>
-            </div>
-
-            <div className="p-5 bg-white rounded-md shadow-md">
-              <img
-                src={Rentify}
-                className="h-[10rem]  w-[100%] flex justify-center items-center"
-              />
-              <h3 className="text-2xl font-medium ">Rentify</h3>
-              <p className=" text-base">
-                It is Rental House Booking Website where users can rent a house
-                and also sellers can update the property
-              </p>
-            </div>
-          </div>
-        </div>
+        {projectdata.map((data, index) => (
+          <ProjectCard
+            title={data.title}
+            desc={data.desc}
+            key={index}
+            pid={index}
+            cover={data.coverimg}
+            id={data.id}
+          />
+        ))}
       </div>
     </>
   );
