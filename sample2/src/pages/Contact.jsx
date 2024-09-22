77c3b7589274364d42bbfc77128dc2ab253892a8
Mahalakshmi <mahakgm0827@gmail.com>
2024-09-22 22:51:50 +0530

    changes

--- sample2/src/pages/Contact.jsx
+++ sample2/src/pages/Contact.jsx
@@ -18,37 +18,33 @@ const Contact = () => {
 
   return (
     <div
-      className="min-h-screen w-full h-screen flex items-center justify-center bg-cover bg-center relative overflow-hidden"
+      className="min-h-[calc(100vh-3rem)] h-[80%]w-full flex items-center justify-center bg-cover bg-center relative overflow-hidden"
       style={{
         backgroundImage: `url('https://img.freepik.com/free-photo/luxury-working-room-executive-office_105762-1725.jpg?t=st=1726893567~exp=1726897167~hmac=e1ccd6eb76067eaa97a3824773551e5a81b75702af48cc593832bc9e59e3047d&w=900')`,
       }}
     >
-      <div className="relative p-6 flex flex-col md:flex-row rounded-lg shadow-lg w-[90%] md:w-[70%] lg:w-[60%] backdrop-blur-lg border-2 border-black ">
+      <div className="relative p-3 flex flex-col md:flex-row rounded-lg shadow-lg w-[90%] md:w-[60%] lg:w-[50%] backdrop-blur-lg border-2 border-black">
         {/* Left side with image and details */}
         <div className="w-full md:w-1/2 flex flex-col items-center justify-center p-4">
           <img
             src="https://img.freepik.com/free-photo/business-woman-working-laptop-computer_1150-104.jpg?w=826&t=st=1726895032~exp=1726895632~hmac=8e1d5fd02a2e0765ef4b9f9829b267f5bb3f57e2fe5c2b82f5f62c9d92770847"
             alt="Working Woman"
-            className="w-32 h-32 md:w-40 md:h-40 rounded-full shadow-md mb-4"
+            className="w-24 h-24 md:w-32 md:h-32 rounded-full shadow-md mb-4"
           />
-          <h2 className="font-bold text-blue-600 text-bold text-3xl">
+          <h2 className="font-bold text-blue-600 text-bold text-2xl">
             MAHALAKSHMI K
           </h2>
-          {/* <p className="text-center text-black font-bold">
-            Full-Stack Developer with a passion for creating innovative
-            solutions
-          </p> */}
-          <p className="text-center mt-2  font-bold text-white text-xl">
+          <p className="text-center mt-2 font-bold text-white text-lg">
             Email: Mahakgm0827@gmail.com
           </p>
-          <p className="text-center font-bold text-white text-xl">
+          <p className="text-center font-bold text-white text-lg">
             Phone: +91 9943077813
           </p>
         </div>
 
         {/* Right side with the contact form */}
         <div className="w-full md:w-1/2 p-4">
-          <h1 className="text-3xl font-bold text-center text-blue-600 mb-4">
+          <h1 className="text-2xl font-bold text-center text-blue-600 mb-4">
             Contact Me
           </h1>
           <form onSubmit={handleSubmit} className="space-y-3">
@@ -102,7 +98,7 @@ const Contact = () => {
                 onChange={handleChange}
                 className="w-full p-2 mt-1 border-2 border-blue-300 rounded-lg focus:outline-none focus:border-blue-500"
                 placeholder="Enter your message"
-                rows="4"
+                rows="3"
                 required
               />
             </div>
--- sample2/src/pages/Home.jsx
+++ sample2/src/pages/Home.jsx
@@ -1,17 +1,16 @@
 import ProfileImage from "../assets/img/Mahalakshmi_Profile.jpg";
 import java from "../assets/img/java.png";
 import mysql from "../assets/img/java.png";
-import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
     <>
-      <div className="min-h-screen h-[80%] flex flex-col md:flex-row px-10 py-10 justify-center items-center bg-[#73d6d3] overflow-hidden">
+      <div className="min-h-[calc(100vh-3rem)] h-[80%] flex flex-col md:flex-row px-10 py-10 justify-center items-center bg-[#73d6d3] overflow-hidden">
         <div className="md:w-1/2 flex justify-center items-center">
           <img
             src={ProfileImage}
             alt="Profile"
-            className="rounded-full h-3/5 w-3/5 bg-white/35  shadow-[0_10px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-md border-white/20"
+            className="rounded-full h-3/5 w-3/5 bg-white/35 shadow-[0_10px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-md border-white/20"
           />
         </div>
         <div className="md:w-1/2 flex flex-col justify-center gap-2">
@@ -20,7 +19,7 @@ const Home = () => {
           </h1>
 
           <p className="font-medium text-base font-[Poppins] justify-left">
-            I am Good in Java,MySQL,HTML,CSS,TailWind CSS.
+            I am Good in Java, MySQL, HTML, CSS, TailWind CSS.
           </p>
           <p className="font-medium text-base font-[Poppins] justify-left">
             I am Currently Learning MERN Stack
